901f26a7f852717891409849bf50e6c6b8a5622e
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-11-28 12:25:34 +0300

    Add files via upload

---
+++ list-ogrenme-836/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace list_ogrenme_836
+{
+    public partial class Form1 : Form
+    {
+        List<int> Sayilar = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtS1.Text);
+            Sayilar.Add(sayi);
+
+            lbSayilar.DataSource=Sayilar.ToList();
+        }
+    }
+}
